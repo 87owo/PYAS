0627ef7814fb55dfe6f597a6e0c02b1d3837e785
87owo <85057800+87owo@users.noreply.github.com>
2022-09-22 18:14:53 +0800

    Add files via upload

--- Version_File.py
+++ Version_File.py
@@ -1,10 +1,10 @@
 import pyinstaller_versionfile
 pyinstaller_versionfile.create_versionfile(
     output_file="versionfile.txt",
-    version='2.4.2',
+    version='2.4.3',
     company_name="PYAS Security",
     file_description="Python Antivirus Software",
     internal_name="PYAS",
-    legal_copyright="Copyright© 2020-2022 PYAS",
+    legal_copyright="© PYAS Security",
     original_filename="PYAS.exe",
     product_name="PYAS")

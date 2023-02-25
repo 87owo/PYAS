c46a315280e6445fa51ae68f7ad5fc758fc7fc64
PYDT <85057800+87owo@users.noreply.github.com>
2023-02-25 19:08:40 +0800

    Add files via upload

--- PYAS_Version_File.py
+++ PYAS_Version_File.py
@@ -1,7 +1,7 @@
 import pyinstaller_versionfile
 pyinstaller_versionfile.create_versionfile(
     output_file="versionfile.txt",
-    version='2.5.8',
+    version='2.5.9',
     company_name="PYAS Security",
     file_description="Python Antivirus Software",
     internal_name="PYAS",

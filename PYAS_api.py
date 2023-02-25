fa721e2e5140af50685893c9dd26acc699b4a08f
PYDT <85057800+87owo@users.noreply.github.com>
2023-02-25 19:09:12 +0800

    Add files via upload

---
+++ PYAS_api.py
@@ -0,0 +1,13 @@
+import requests
+
+def api_scan(types,text):
+    response = requests.get("http://27.147.30.238:5001/pyas", params={types: text})
+    if response.status_code == 200:
+        return response.text
+    else:
+        return response.status_code
+
+print(scan("md5","<md5 hashes>"))
+
+# http://27.147.30.238:5001/pyas?md5=<md5 hashes>
+# return string True, False

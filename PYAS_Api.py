35f7d6d71ee95240d3b282275aaa25b7cc2c035e
PYDT <85057800+87owo@users.noreply.github.com>
2023-03-06 18:43:50 +0800

    Add files via upload

--- PYAS_Api.py
+++ PYAS_Api.py
@@ -1,7 +1,7 @@
 import requests
 
 def scan(types,text):
-    response = requests.get("https://api.pyas.cf/pyas", params={types: text})
+    response = requests.get("http://27.147.30.238:5001/pyas", params={types: text})
     if response.status_code == 200:
         if response.text == "True":
             return True
@@ -11,6 +11,3 @@ def scan(types,text):
         return response.status_code
 
 print(scan("md5","<md5 hashes>"))
-
-# https://api.pyas.cf/pyas?md5=<md5 hashes>
-# return string True, False

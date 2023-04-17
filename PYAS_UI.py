9366cb3de9c0d27c65ec268324eb348e594bfa4e
PYDT <85057800+87owo@users.noreply.github.com>
2023-04-17 11:48:57 +0800

    Add files via upload

--- PYAS_UI.py
+++ PYAS_UI.py
@@ -11,7 +11,7 @@ class Ui_MainWindow(object):
         MainWindow.setFont(font)
         MainWindow.setFocusPolicy(QtCore.Qt.TabFocus)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("Library/PYAS/Icon/ICON.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("Library/ICON.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         MainWindow.setIconSize(QtCore.QSize(24, 24))
         MainWindow.setToolButtonStyle(QtCore.Qt.ToolButtonIconOnly)

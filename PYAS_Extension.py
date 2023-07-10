475d4b4a80cc6c118e21b1d75fc2dc32ac3f970e
PYDT <85057800+87owo@users.noreply.github.com>
2023-07-10 21:36:31 +0800

    Add files via upload

--- PYAS_Extension.py
+++ PYAS_Extension.py
@@ -3,30 +3,21 @@ flist = [".exe",".dll",".com",".msi",".scr",
          ".jar",".htm",".html",".ppt",".pptx",
          ".pdf",".xls",".xlsx",".doc",".docx"]
 
-plist = [".jpg",".jpeg",".png",".webp",".gif",
-         ".bmp",".ico",".tif",".tiff",".cur"]
-
-mlist = [".mp3",".wav",".aac",".flac",".alac",
-         ".wma",".aiff",".ogg",".dsd",".pcm"]
-
-vlist = [".mp4",".avi",".mov",".wmv",".flv",
-         ".wpl",".avchd",".mkv",".f4v",".swf"]
-
-tlist = [".txt",".rtf",".ppt",".pptx",".pdf",
-         ".xls",".xlsx",".doc",".docx",".wps"]
-
-zlist = [".zip",".rar",".7z",".iso",".img",
-         ".bz2",".gz",".tar",".jgz",".txz"]
-
-slist = [".exe",".dll",".com",".msi",".sys",
-         ".inf",".ini",".scr",".log",".old"]
-
-blist = [".vid",".vhd",".vmx",".vsv",".ova",
-         ".vmdk",".qcow2",".vbox",".vmcx"]
-
-rlist = [".bat",".cmd",".ps1",".vbs",".reg",
+alist = [".jpg",".jpeg",".png",".webp",".gif", #picture
+         ".bmp",".ico",".tif",".tiff",".cur",
+         ".mp3",".wav",".aac",".flac",".alac", #music
+         ".wma",".aiff",".ogg",".dsd",".pcm",
+         ".mp4",".avi",".mov",".wmv",".flv",   #video
+         ".wpl",".avchd",".mkv",".f4v",".swf",
+         ".txt",".rtf",".ppt",".pptx",".pdf",  #text
+         ".xls",".xlsx",".doc",".docx",".wps",
+         ".zip",".rar",".7z",".iso",".img",    #compression
+         ".bz2",".gz",".tar",".jgz",".txz",
+         ".exe",".dll",".com",".msi",".sys",   #system
+         ".inf",".ini",".scr",".log",".old",
+         ".vid",".vhd",".vmx",".vsv",".ova",   #virtual
+         ".vmdk",".qcow2",".vbox",".vmcx",
+         ".bat",".cmd",".ps1",".vbs",".reg",   #program
          ".jar",".java",".swift",".py",".e",
          ".c",".cpp",".h",".rb",".go",".sql",
          ".php",".htm",".html","css",".js"]
-
-alist = plist+mlist+vlist+tlist+zlist+slist+blist+rlist

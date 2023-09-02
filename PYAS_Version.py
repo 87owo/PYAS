5633e70a11254bdea4a10457e0e55199cba64e54
PYDT <85057800+87owo@users.noreply.github.com>
2023-09-02 10:38:25 +0800

    Add files via upload

--- PYAS_Version.py
+++ PYAS_Version.py
@@ -6,5 +6,5 @@ company_name="PYAS Security",
 file_description="Python Antivirus Software",
 internal_name="PYAS",
 legal_copyright="© PYAS Security",
-original_filename="󠁅PYAS.exe",
+original_filename="PYAS.exe",
 product_name="PYAS")

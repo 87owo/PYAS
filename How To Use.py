d5327c6370033e6a429b938f7e6bf990d34de337
PYDT <85057800+87owo@users.noreply.github.com>
2024-01-15 09:39:35 +0800

    Delete How To Use.py

--- How To Use.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-file_path = input("input scan file: ")
-result = subprocess.check_output(["PYAS_Engine.exe", file_path], text=True, stderr=subprocess.STDOUT)
-print(result.strip())

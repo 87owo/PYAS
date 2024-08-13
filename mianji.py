2ab78fe1a51f71a0f2e46356b71776790c315287
SYSTEM-WIN-ZDY <hhfjhfuhj@outlook.com>
2024-08-13 10:43:48 +0800

    一个小游戏    a game
    一个小游戏

---
+++ mianji.py
@@ -0,0 +1,33 @@
+# 用python写一个小游戏
+import random as ra
+
+
+
+b = ra.randint(0,100)
+
+a = 5
+while a > 0:
+    temp = input("猜一下我心里想的是什么数字")
+    guess = int(temp)
+    a = a - 1
+    if guess ==b:
+        print("猜对了也没奖励")
+        a = 0
+    else:
+        if guess < b:
+            print("小了~")
+        else:
+            print("大了~")
+
+print("不玩了^-^")
+
+import xl
+import random
+
+xl.shark(9)
+lucky = random.randint(1, 9)
+for i in range(9):
+    if xl.click() == lucky:
+        xl.gameover('shark')
+        print("你输了！！！！！！！！")
+

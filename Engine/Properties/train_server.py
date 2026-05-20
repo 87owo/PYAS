0ab876633f5608b0b25602b3387193065d9a0157
87owo <85057800+87owo@users.noreply.github.com>
2026-05-21 00:27:01 +0800

    Add files via upload

--- Engine/Properties/train_server.py
+++ Engine/Properties/train_server.py
@@ -143,7 +143,7 @@ def train_process(X, y, feature_names):
     n_neg = (y_train == 0).sum()
     
     base_weight = n_neg / n_pos if n_pos > 0 else 1.0
-    weight_ratio_target = 0.003
+    weight_ratio_target = 0.005
     final_pos_weight = base_weight * weight_ratio_target
     
     print(f"[*] Sample distribution: Safe={n_neg}, Malware={n_pos}")

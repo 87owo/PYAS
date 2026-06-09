130b040de116a8a662880e82bae39ba7b704617a
87owo <85057800+87owo@users.noreply.github.com>
2026-06-09 16:08:44 +0800

    Add files via upload

--- Interface/static/js/main.js
+++ Interface/static/js/main.js
@@ -447,23 +447,23 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     const checkVirusListEmpty = () => {
-    updateCustomSelectUI('scan_method_select', 'none');
-    if (appState.virusResults.length === 0) {
-        appState.virusMap.clear();
-        changeScanSelectMode('scan');
-        const title = document.querySelector('#scan_window .section-title');
-        const text = document.getElementById('progress_text');
-        if (title) { 
-            title.setAttribute('data-i18n', 'scan_virus'); 
-            title.textContent = getMsg('scan_virus'); 
+        updateCustomSelectUI('scan_method_select', 'none');
+        if (appState.virusResults.length === 0) {
+            appState.virusMap.clear();
+            changeScanSelectMode('scan');
+            const title = document.querySelector('#scan_window .section-title');
+            const text = document.getElementById('progress_text');
+            if (title) { 
+                title.setAttribute('data-i18n', 'scan_virus'); 
+                title.textContent = getMsg('scan_virus'); 
+            }
+            if (text) {
+                text.setAttribute('data-i18n', 'scan_virus_desc');
+                text.removeAttribute('data-dynamic-msg');
+                text.textContent = getMsg('scan_virus_desc');
+            }
         }
-        if (text) {
-            text.setAttribute('data-i18n', 'scan_virus_desc');
-            text.removeAttribute('data-dynamic-msg');
-            text.textContent = getMsg('scan_virus_desc');
-        }
-    }
-};
+    };
 
     const triggerScan = (method) => {
         if (!window.pywebview) return;

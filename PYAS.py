8a0209a99ac7af0703d7fd0d20b68b2ded63d493
87owo <85057800+87owo@users.noreply.github.com>
2026-09-04 13:10:39 +0800

    Add files via upload

--- PYAS.py
+++ PYAS.py
@@ -366,7 +366,7 @@ class _MainMixin:
         self.lock_io = threading.RLock()
 
         self.pyas_default = {
-            "version": "3.6.8",
+            "version": "3.6.9",
             "api_host": "https://pyas-security.com/",
             "api_key": "fBRZxYS1UxykM-qzNOlKOEl63WILzlvgNMn6QfsG6FXCAAIktCrOPTAfY5_hEyuZ",
             "suffix": [".exe", ".dll", ".sys", ".ocx", ".scr", ".efi", ".acm", ".ax", ".cpl", ".drv", ".com", ".mui", ".pyd", ".wfx", ".api", ".awx", ".rll", ".winmd"],
